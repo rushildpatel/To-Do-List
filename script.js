0fc72dafb4432fdc3db9dda527ab81aef747717a
Your Name <you@example.com>
2023-08-01 18:16:08 +0530

    new buttons sort and cleanup added with their functionality

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 let btnAdd = $("#btnAdd");
-let btnClear = $("#btnClear");
+let btnReset = $("#btnReset");
+let btnCLeanup = $("#btnCleanup");
 let ulTasks = $("#ulTasks");
 let inpNewTask = $("#inpNewTask");
+let btnSort = $("#btnSort");
 
-btnAdd.click(() => {
+function addItem() {
   let listItem = $("<li>", {
     class: "list-group-item",
     text: inpNewTask.val(),
   });
 
+  listItem.click(() => {
+    // console.log("clicked");
+    listItem.toggleClass("done");
+  });
+
   ulTasks.append(listItem);
-  console.log(inpNewTask.val());
+  // console.log(inpNewTask.val());
   inpNewTask.val("");
+}
+
+function clearDone() {
+  $("#ulTasks .done").remove();
+}
+
+function sortTasks() {
+  $("#ulTasks .done").appendTo(ulTasks);
+}
+
+inpNewTask.keypress((e) => {
+  if (e.which == 13) {
+    addItem();
+  }
 });
+
+btnCLeanup.click(clearDone);
+
+btnAdd.click(addItem);
+
+btnReset.click(() => inpNewTask.val(""));
+
+btnSort.click(sortTasks);
